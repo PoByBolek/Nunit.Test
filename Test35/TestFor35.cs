e9a8313006cab40123b3aa337f5cabe6c688eb37
Florian Zimmermann <flo@chaos-wg.net>
2019-08-09 15:24:20 +0200

    add the test files

---
+++ Test35/TestFor35.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace Test35
+{
+    [TestFixture]
+    public class TestFor35
+    {
+        [Test]
+        public void Example()
+        {
+            Assert.AreEqual(new Version(2, 0, 0, 0), typeof(int).Assembly.GetName().Version);
+        }
+    }
+}
---
+++ Test40/TestFor40.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace Test40
+{
+    [TestFixture]
+    public class TestFor40
+    {
+        [Test]
+        public void Example()
+        {
+            Assert.AreEqual(new Version(4, 0, 0, 0), typeof(int).Assembly.GetName().Version);
+        }
+    }
+}
